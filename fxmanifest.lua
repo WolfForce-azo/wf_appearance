c6d57ad699761992ae0556acc9bd7592961d7564
WolfForce-azo <abdulazizkavak@icloud.com>
2025-03-10 16:26:22 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game "gta5"
 author "snakewiz & iLLeniumStudios"
 description "A flexible player customization script for FiveM servers."
 repository "https://github.com/iLLeniumStudios/illenium-appearance"
-version "1.0"
+version "1.1"
 
 lua54 "yes"
 
